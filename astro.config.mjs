d3c47ef4f39938c40dc26fe57d37259d5a58e83e
rainkaze <rainkaze@qq.com>
2026-06-09 14:37:54 +0800

    🔧 wrench: 添加 Preact 集成

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
   site: "https://rainkaze-test.netlify.app",
+  integrations: [preact()],
 });
